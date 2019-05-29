61d1297d6a0afa5e645eae8beaf4c030b5dcf6fe
Ray Saltrelli <raymond.saltrelli@protonmail.com>
2019-05-28 21:12:06 -0400

    Add cancel button

--- main.go
+++ main.go
@@ -12,12 +12,8 @@ func main() {
 	http.HandleFunc("/view/", lib.MakeHandler(lib.View))
 	http.HandleFunc("/edit/", lib.MakeHandler(lib.Edit))
 	http.HandleFunc("/save/", lib.MakeHandler(lib.Save))
-
-	content := http.FileServer(http.Dir("content"))
-	http.Handle("/content/", http.StripPrefix("/content", content))
-
-	scripts := http.FileServer(http.Dir("scripts"))
-	http.Handle("/scripts/", http.StripPrefix("/scripts", scripts))
+	http.Handle("/content/", http.StripPrefix("/content/", http.FileServer(http.Dir("content"))))
+	http.Handle("/scripts/", http.StripPrefix("/scripts/", http.FileServer(http.Dir("scripts"))))
 	http.HandleFunc("/", lib.FrontPage)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
